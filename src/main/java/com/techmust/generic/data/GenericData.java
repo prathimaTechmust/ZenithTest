5a5692185626b5f1d4794187b4211a1017b7ffb8
Arshad <arshad@techmust.com>
2019-12-03 13:47:00 +0530

    optimized speed of fetching of RowCount data

--- src/main/java/com/techmust/generic/data/GenericData.java
+++ src/main/java/com/techmust/generic/data/GenericData.java
@@ -577,10 +577,10 @@ public abstract class GenericData implements IGenericData, Serializable
 	        oCriteriaQuery.select(oStudentInformationRoot);
 	        oCriteriaQuery.where(oCriteriaBuilder.equal(oStudentInformationRoot.get("m_nStudentId"), oStudentData.getM_nStudentId()));
 	        oStudentInformationData = oEntityManager.createQuery(oCriteriaQuery).getSingleResult();
-//	        if(oStudentData.getM_nAcademicYearId() > 0)
-//	        	//oStudentInformationData.setM_oAcademicDetails(getAcademicDetails(oStudentData));
-//	        if(oStudentData.getM_nAcademicYearId() > 0)
-//	        	//oStudentInformationData.setM_oZenithScholarshipDetails(getZenithScholarshipDetails(oStudentData));
+	        if(oStudentData.getM_nAcademicYearId() > 0)
+	        	oStudentInformationData.setM_oAcademicDetails(getAcademicDetails(oStudentData));
+	        if(oStudentData.getM_nAcademicYearId() > 0)
+	        	oStudentInformationData.setM_oZenithScholarshipDetails(getZenithScholarshipDetails(oStudentData));
 		}
 		catch (Exception oException)
 		{
@@ -1185,8 +1185,8 @@ public abstract class GenericData implements IGenericData, Serializable
 		        oStudentInformationData = arrStudentList.get(0);
 		        oStudentInformationData.setM_nAcademicYearId(oStudentData.getM_nAcademicYearId());
 		        oStudentInformationData.setM_strStatus(oStudentData.getM_strStatus());
-//		        oStudentInformationData.setM_oAcademicDetails(getAcademicDetails(oStudentInformationData));
-//		        oStudentInformationData.setM_oZenithScholarshipDetails(getSearchStudentUID(oStudentInformationData));
+		        oStudentInformationData.setM_oAcademicDetails(getAcademicDetails(oStudentInformationData));
+		        oStudentInformationData.setM_oZenithScholarshipDetails(getSearchStudentUID(oStudentInformationData));
 	        }						
 		}
 		catch (Exception oException)
